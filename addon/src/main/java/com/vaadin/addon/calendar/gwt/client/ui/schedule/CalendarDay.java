03c9cca95e529bf8c4d06503e5fef378055068a2
Jonatan Kronqvist <jonatan@vaadin.com>
2013-01-22 14:21:56 +0200

    Added license headers through license-maven-plugin

--- addon/src/main/java/com/vaadin/addon/calendar/gwt/client/ui/schedule/CalendarDay.java
+++ addon/src/main/java/com/vaadin/addon/calendar/gwt/client/ui/schedule/CalendarDay.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2010 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Add-On License 2.0
+ * (CVALv2) or GNU Affero General Public License (version 3 or later at
+ * your option).
+ *
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
+ *
+ * You should have received a copy of the license along with this program.
+ * If not, see <http://vaadin.com/license/cval-2.0> or
+ * <http://www.gnu.org/licenses> respectively.
+ */
 package com.vaadin.addon.calendar.gwt.client.ui.schedule;
 
 /**
@@ -10,7 +24,8 @@ public class CalendarDay {
     private int dayOfWeek;
     private int week;
 
-    public CalendarDay(String date, String localizedDateFormat, int dayOfWeek, int week) {
+    public CalendarDay(String date, String localizedDateFormat, int dayOfWeek,
+            int week) {
         super();
         this.date = date;
         this.localizedDateFormat = localizedDateFormat;
--- addon/src/test/java/com/vaadin/addon/calendar/test/HiddenFwdBackButtons.java
+++ addon/src/test/java/com/vaadin/addon/calendar/test/HiddenFwdBackButtons.java
@@ -1,4 +1,35 @@
+/**
+ * Copyright (C) 2010 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Add-On License 2.0
+ * (CVALv2) or GNU Affero General Public License (version 3 or later at
+ * your option).
+ *
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
+ *
+ * You should have received a copy of the license along with this program.
+ * If not, see <http://vaadin.com/license/cval-2.0> or
+ * <http://www.gnu.org/licenses> respectively.
+ */
 package com.vaadin.addon.calendar.test;
+
+/*
+ * #%L
+ * Vaadin Calendar
+ * %%
+ * Copyright (C) 2010 - 2013 Vaadin Ltd
+ * %%
+ * This program is available under GNU Affero General Public License (version
+ * 3 or later at your option).
+ * 
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 
 import java.util.Date;
 import java.util.Locale;
